d1d3e07bca05846cb80e9232783580c4d3041e18
sebastrh <sebastrh23@gmail.com>
2026-09-22 08:59:58 -0600

    Modificaciones en Clase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+import { IonHeader,IonContent,IonButton, IonFooter } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader,IonContent,IonButton,IonFooter],
 })
+
+//Aqui va toda la logica de la página
+// Variables y Metodos
+
 export class HomePage {
+  public numero: number = 0;
   constructor() {}
 }
